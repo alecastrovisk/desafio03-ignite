2fcffa73d7bf5a8bc5203cf823919bcd96ce03a3
alecastrovisk <alexandre.acrsf@gmail.com>
2023-04-27 08:44:50 -0300

    feat: Orgs register

---
+++ src/repositories/orgs-repository.ts
@@ -0,0 +1,6 @@
+import { Org, Prisma } from '@prisma/client'
+
+export interface OrgsRepository {
+  create(data: Prisma.OrgCreateWithoutPetsInput): Promise<Org>
+  findByEmail(email: string): Promise<Org>
+}
--- src/use-cases/org-register.ts
+++ src/use-cases/org-register.ts
@@ -1,5 +1,48 @@
-export class OrgRegisterUseCase {
-  constructor() {}
+import { Org } from '@prisma/client'
+import { OrgsRepository } from '../repositories/orgs-repository'
+import { hash } from 'bcrypt'
+import { UserAlreadyExistsError } from './errors/user-aready-exists-error'
 
-  async execute() {}
+interface OrgRegisterUseCaseRequest {
+  adress: string
+  cep?: string | null
+  phone: string
+  email: string
+  password: string
+}
+
+interface OrgRegisterUseCaseResponse {
+  org: Org
+}
+
+export class OrgRegisterUseCase {
+  constructor(private orgsRepository: OrgsRepository) {}
+
+  async execute({
+    adress,
+    email,
+    password,
+    phone,
+    cep,
+  }: OrgRegisterUseCaseRequest): Promise<OrgRegisterUseCaseResponse> {
+    const password_hash = await hash(password, 6)
+
+    const userWithSameEmail = await this.orgsRepository.findByEmail(email)
+
+    if (userWithSameEmail) {
+      throw new UserAlreadyExistsError()
+    }
+
+    const org = await this.orgsRepository.create({
+      adress,
+      email,
+      password_hash,
+      phone,
+      cep,
+    })
+
+    return {
+      org,
+    }
+  }
 }
